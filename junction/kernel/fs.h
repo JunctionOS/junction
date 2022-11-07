1c3f7264fae117305fc68107f90828aa80688a92
Adam Belay <adam@abelay.me>
2022-11-07 17:49:10 +0000

    more notes on what an inode should be

--- junction/kernel/fs.h
+++ junction/kernel/fs.h
@@ -2,9 +2,17 @@
 
 #pragma once
 
+extern "C" {
+#include <sys/stat.h>
+}
+
 #include <memory>
+#include <string_view>
+#include <vector>
 
 #include "junction/kernel/file.h"
+
+namespace junction {
 
 class Inode {
  public:
@@ -13,8 +21,14 @@ class Inode {
   virtual std::shared_ptr<File> Open(uint32_t mode, uint32_t flags);
   // Lookup a child inode for this inode (can be empty).
   virtual std::shared_ptr<Inode> Lookup(const std::string_view &name);
-  // Insert a new child inode into this inode (type must be directory).
-  virtual void Insert(const std::string_view &name, std::shared_ptr<Inode> inode);
+  // Insert a new child inode into this inode (must be a directory).
+  virtual Status<void> Insert(const std::string_view &name, std::shared_ptr<Inode> inode);
+  // Remove a child inode from this inode (must be a directory).
+  virtual Status<void> Remove(const std::string_view &name);
+  // Adjust the size of the file.
+  virtual Status<void> Truncate(size_t newlen);
+  // Get the unix stat structure.
+  virtual Status<stat> Stat();
 }
 
 class FileSystem {
@@ -24,3 +38,5 @@ class FileSystem {
   std::shared_ptr<Inode> root_;  // the root directory of the file system
   std::shared_ptr<Inode> cwd_;   // the current working directory
 };
+
+}  // namespace junction
