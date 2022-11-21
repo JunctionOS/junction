cd49c8d5107a4cd876273ea0b04b3d79800981fd
Gohar Irfan Chaudhry <girfan@mit.edu>
2022-11-21 07:53:51 -0500

    Bounds check on fd before removing from FileTable

--- junction/kernel/file.cc
+++ junction/kernel/file.cc
@@ -93,14 +93,13 @@ void FileTable::InsertAt(int fd, std::shared_ptr<File> f) {
 
 bool FileTable::Remove(int fd) {
   rt::SpinGuard g(&lock_);
-
+    
   // Check if the file is present.
+  if (fd >= farr_->len) return false;
   if (!farr_->files[fd]) return false;
 
   // Remove the file.
   farr_->files[fd].reset();
-  if (static_cast<size_t>(fd) != farr_->len - 1) return true;
-
   return true;
 }
 
