8193b02119e924ac9e369d0fcc3d2541fb4ba583
Adam Belay <adam@abelay.me>
2023-01-04 18:46:27 +0000

    linter fixes

--- scripts/run_clang_tidy.sh
+++ scripts/run_clang_tidy.sh
@@ -4,4 +4,4 @@ set -xe
 # Globals
 CODE_DIR=junction
 
-find ${CODE_DIR} -name '*.cc' | xargs clang-tidy -p build -header-filter=".*" -extra-arg=-std=gnu++20 --config-file=.clang-tidy
+find ${CODE_DIR} -name '*.cc' | xargs run-clang-tidy -j12 -p build -header-filter=".*" -extra-arg=-std=gnu++20 -config-file .clang-tidy
