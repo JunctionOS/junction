db95a45015ceb09dc11cb657aa62cc0dc26c6e8e
Gohar Irfan Chaudhry <girfan@mit.edu>
2023-02-15 20:21:32 -0500

    git submodule remote

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,7 +14,7 @@ sudo apt install -y clang-format clang-tidy perl libboost-dev libboost-program-o
 sudo apt install -y make gcc cmake pkg-config libnl-3-dev libnl-route-3-dev libnuma-dev uuid-dev libssl-dev libaio-dev libcunit1-dev libclang-dev libncurses-dev meson python3-pyelftools
 
 # Initialize submodules
-git submodule update --init --recursive
+git submodule update --init --recursive --remote
 
 # Install modules
 cd $SCRIPT_DIR
