dff4b13385f5c93c70e82612d4094fe85bbb39ac
Gohar Irfan Chaudhry <girfan@mit.edu>
2023-02-22 22:25:43 -0500

    GitHub Actions (#26)

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,12 +6,10 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 ROOT_DIR=${SCRIPT_DIR}/../
 
 # Install Linux packages
-#sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt update
-#sudo apt install -y g++-11
-sudo apt install -y clang-format clang-tidy perl libboost-dev libboost-program-options-dev bison
+sudo apt install -y clang-format clang-tidy perl libboost-dev libboost-program-options-dev bison gcc-12 g++-12
 # From caladan README
-sudo apt install -y make gcc cmake pkg-config libnl-3-dev libnl-route-3-dev libnuma-dev uuid-dev libssl-dev libaio-dev libcunit1-dev libclang-dev libncurses-dev meson python3-pyelftools
+sudo apt install -y make cmake pkg-config libnl-3-dev libnl-route-3-dev libnuma-dev uuid-dev libssl-dev libaio-dev libcunit1-dev libclang-dev libncurses-dev meson python3-pyelftools
 
 # Initialize submodules
 git submodule update --init --recursive
