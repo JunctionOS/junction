6afc1384bcdf51ba93bfa000a6b6a0111f925742
Adam Belay <adam@abelay.me>
2023-04-30 21:18:57 +0000

    add string splitting utility function

--- junction/base/string.h
+++ junction/base/string.h
@@ -2,22 +2,33 @@
 
 #pragma once
 
-#include <string>
 #include <string_view>
+#include <vector>
 
 namespace junction {
 
-// Checks if a string ends with another string.
-inline bool ends_with(const std::string_view& value,
-                      const std::string_view& ending) {
+// ends_with checks if a string ends with another string.
+inline bool ends_with(std::string_view value, std::string_view ending) {
   if (ending.size() > value.size()) return false;
   return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
-inline bool starts_with(const std::string_view& value,
-                        const std::string_view& starting) {
+// starts_with checks if a string starts with another string
+inline bool starts_with(std::string_view value, std::string_view starting) {
   if (starting.size() > value.size()) return false;
   return std::equal(starting.begin(), starting.end(), value.begin());
+}
+
+// split creates a vector of string views delineated by a seperator character
+inline std::vector<std::string_view> split(std::string_view text, char sep) {
+  std::vector<std::string_view> tokens;
+  std::string_view::size_type start = 0, end = 0;
+  while ((end = text.find(sep, start)) != std::string_view::npos) {
+    tokens.push_back(text.substr(start, end - start));
+    start = end + 1;
+  }
+  tokens.push_back(text.substr(start));
+  return tokens;
 }
 
 }  // namespace junction
