f8ed11b2ac385270371b8e0d9d684a447d173f7b
Adam Belay <adam@abelay.me>
2023-04-30 17:12:27 +0000

    add full inode API to notes

--- junction/kernel/fs.h
+++ junction/kernel/fs.h
@@ -36,6 +36,7 @@ constexpr unsigned int kTypeFIFO = S_IFIFO;
 // TODO: These are notes on a new file system design (WIP)
 //
 
+// Inode is the base class for all inodes
 class Inode : std::enable_shared_from_this<Inode> {
  public:
   Inode(unsigned int type, unsigned long (inum) : type_(type), inum_(inum) {}
@@ -54,36 +55,69 @@ class Inode : std::enable_shared_from_this<Inode> {
   const unsigned long inum_;  // inode number
 };
 
+// ISoftLink is an inode type for soft links
+class ISoftLink : public Inode {
+ public:
+  // Opens a file that does nothing.
+  std::shared_ptr<File> Open(uint32_t mode, uint32_t flags) override;
+
+  // ReadLink reads the path of the link (if this is a softlink inode).
+  virtual Status<std::string> ReadLink();
+};
+
 struct dir_entry {
   std::string name;
   unsigned long inum;
   unsigned int type;
 };
 
+// IDir is an inode type for directories
 class IDir : public Inode {
  public:
-  // Opens a file that supports getdents64().
+  // Opens a file that supports getdents() and getdents64().
   std::shared_ptr<File> Open(uint32_t mode, uint32_t flags) override;
 
-  // Lookup an inode in this directory (can be empty).
-  virtual std::shared_ptr<Inode> Lookup(std::string_view name) = 0;
-  // Insert an inode into this directory.
-  virtual Status<void> Insert(std::string_view name,
-                              std::shared_ptr<Inode> inode) = 0;
-  // Remove an inode from this directory.
-  virtual Status<void> Remove(std::string_view name) = 0;
-  // Create a new directory.
-  virtual Status<void> MakeDir(std::string_view name, uint32_t mode) = 0;
-  // Create a new file.
+  // TODO(amb): Add an API to help handle Stat
+
+  // Lookup finds a directory entry by name.
+  virtual Status<std::shared_ptr<Inode>> Lookup(std::string_view name) = 0;
+  // MkNod creates a file (usually of a special type).
+  virtual Status<void> MkNod(std::string_view name, mode_t mode, dev_t dev) = 0;
+  // MkDir creates a directory.
+  virtual Status<void> MkDir(std::string_view name, mode_t mode) = 0;
+  // Unlink removes a file.
+  virtual Status<void> Unlink(std::string_view name) = 0;
+  // RmDir remove a directory.
+  virtual Status<void> RmDir(std::string_view name) = 0;
+  // SymLink creates a symbolic link.
+  virtual Status<void> SymLink(std::string_view name,
+                               std::string_view path) = 0;
+  // Rename changes the name/location of a file (called on the dest IDir).
+  virtual Status<void> Rename(IDir &src, std::string_view src_name,
+                              std::string_view dst_name) = 0;
+  // Link creates a hard link.
+  virtual Status<void> Link(INode &node, std::string_view name) = 0;
+  // Create a new normal file.
   virtual Status<std::shared_ptr<File>> Create(std::string_view name,
                                                uint32_t mode) = 0;
+
+  // If true, don't allow creation of new directory entries.
+  [[nodiscard]] bool is_dead() const { return dead_; }
+  // Get a reference to the parent of this directory.
+  [[nodiscard]] std::shared_ptr<IDir> get_parent() const { return parent_; }
 
  private:
   // GetDents returns a vector of the current entries.
   virtual std::vector<dir_entry> GetDents() = 0;
+
+  // Parent directory.
+  std::shared_ptr<IDir> parent_;
+  // Directory has been removed.
+  bool dead_;
 };
 
-class FileSystem {
+// FSRoot manages the root directory and namespace for a process
+class FSRoot {
  private:
   std::shared_ptr<IDir> root_;
   std::shared_ptr<IDir> cwd_;
