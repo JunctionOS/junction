1c7c3628ce4d8baccdb5a51aa8a7d04f036e6e49
Adam Belay <adam@abelay.me>
2023-05-01 15:17:42 +0000

    more directory walking code

--- junction/fs/core.cc
+++ junction/fs/core.cc
@@ -14,20 +14,17 @@ constexpr bool NameIsValid(std::string_view name) {
                       [](char c) { return c == '/' || c == '\0'; });
 }
 
-Status<std::shared_ptr<Inode>> LookupPath(std::shared_ptr<Inode> pos,
-                                          std::string_view path) {
-  std::vector<std::string_view> spath = split(path, '/');
-  if (spath.size() == 0) return MakeError(EINVAL);
-
+Status<std::shared_ptr<Inode>> LookupInode(
+    std::shared_ptr<Inode> pos, const std::vector<std::string_view> &spath) {
   for (std::string_view v : spath) {
-    if (pos->get_type() != kFlagDirectory) return MakeError(ENOTDIR);
-    auto dir = std::static_pointer_cast<IDir>(pos);
+    if (pos->get_type() != kTypeDirectory) return MakeError(ENOTDIR);
+    auto &dir = static_cast<IDir &>(*pos);
     if (v == ".") continue;
     if (v == "..") {
-      pos = dir->get_parent();
+      pos = dir.get_parent();
       continue;
     }
-    Status<std::shared_ptr<Inode>> ret = dir->Lookup(v);
+    Status<std::shared_ptr<Inode>> ret = dir.Lookup(v);
     if (!ret) return MakeError(ret);
     pos = std::move(*ret);
   }
@@ -35,13 +32,46 @@ Status<std::shared_ptr<Inode>> LookupPath(std::shared_ptr<Inode> pos,
   return pos;
 }
 
+Status<std::tuple<std::shared_ptr<IDir>, std::string_view>> LookupIDir(
+    std::shared_ptr<Inode> pos, std::string_view path) {
+  std::vector<std::string_view> spath = split(path, '/');
+  if (spath.empty()) return MakeError(EINVAL);
+
+  std::string_view name = spath.back();
+  spath.pop_back();
+  if (!NameIsValid(name)) return MakeError(EINVAL);
+
+  Status<std::shared_ptr<Inode>> ret = LookupInode(pos, spath);
+  if (!ret) return MakeError(ret);
+  if ((*ret)->get_type() != kTypeDirectory) return MakeError(ENOTDIR);
+  return std::make_tuple(std::static_pointer_cast<IDir>(std::move(*ret)), name);
+}
+
+Status<std::tuple<std::shared_ptr<IDir>, std::string_view>> LookupIDir(
+    const FSRoot &root, std::string_view path) {
+  std::shared_ptr<IDir> pos = path[0] == '/' ? root.get_root() : root.get_cwd();
+  return LookupIDir(pos, path);
+}
+
 }  // namespace
 
-Status<std::shared_ptr<Inode>> FSLookup(const FSRoot &root,
-                                        std::string_view path) {
-  std::shared_ptr<IDir> pos = path[0] == '/' ? root.get_root() : root.get_cwd();
-  Status<std::shared_ptr<Inode>> ret = LookupPath(pos, path);
-  return LookupPath(pos, path);
+//
+// System call implementations
+//
+
+#if 0
+int usys_mknod(const char *path, mode_t mode, dev_t dev) {
+  FSRoot root;
+
+  // Find the directory
+  auto ret = LookupIDir(root, path);
+  if (!ret) return MakeCError(ret);
+  auto [idir, name] = *ret;
+
+  // Do mknod
+  if (auto ret = idir->MkNod(name, mode, dev); !ret) return MakeCError(ret);
+  return 0;
 }
+#endif
 
 }  // namespace junction
