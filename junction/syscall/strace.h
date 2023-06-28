1c22a2bfcab5f25a8de84995fd377fcaae87257a
Adam Belay <adam@abelay.me>
2023-06-28 05:34:00 +0000

    use fold expression for strace

--- junction/syscall/strace.h
+++ junction/syscall/strace.h
@@ -7,38 +7,23 @@
 
 namespace junction {
 
-template <class F>
-void do_for(F f) {
-  // Parameter pack is empty.
-}
-
-template <class F, typename First>
-void do_for(F f, First first) {
-  f(first, true);
-  do_for(f);
-}
-
-template <class F, typename First, typename... Rest>
-void do_for(F f, First first, Rest... rest) {
-  f(first, false);
-  do_for(f, rest...);
-}
-
 template <typename... Args>
 void LogSyscall(long retval, std::string_view name, Args... args) {
   std::stringstream ss;
 
   ss << "[" << myproc().get_pid() << ":" << mythread().get_tid() << "] ";
   ss << name << "(";
+  size_t i = 0;
 
-  do_for(
-      [&](auto arg, bool last) {
+  (
+      [&](auto arg) {
         ss << arg;
-        if (!last) ss << ", ";
-      },
-      args...);
+        if (++i != sizeof...(args)) ss << ", ";
+      }(args),
+      ...);
 
   ss << ") = " << retval;
   LOG(INFO) << ss.str();
 }
+
 }  // namespace junction
