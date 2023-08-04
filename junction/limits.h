7411566a40d1d0ab255673de002ddefd7480abd2
Josh Fried <joshuafried@gmail.com>
2023-08-04 16:35:59 +0000

    increase MM limit

--- junction/limits.h
+++ junction/limits.h
@@ -12,7 +12,7 @@ namespace junction {
 // The maximum number of running proccesses.
 inline constexpr size_t kMaxProcesses = 1024;
 // The size in bytes of the most virtual memory a process can map.
-inline constexpr size_t kMemoryMappingSize = (1UL << 34);  // 16 GB
+inline constexpr size_t kMemoryMappingSize = (1UL << 36);  // 64 GB
 // The size in bytes of the pipe's channel.
 inline constexpr size_t kPipeSize = 16 * kPageSize;  // same default as Linux
 // The maximum number of queued RT signals
