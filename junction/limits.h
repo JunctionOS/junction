c3083f881c3f704019685e1dfaeb8c1703bf430c
Josh Fried <joshuafried@gmail.com>
2023-08-08 22:02:23 +0000

    increase max threads

--- junction/limits.h
+++ junction/limits.h
@@ -9,8 +9,8 @@
 
 namespace junction {
 
-// The maximum number of running proccesses.
-inline constexpr size_t kMaxProcesses = 1024;
+// The maximum number of running processes/threads.
+inline constexpr size_t kMaxProcesses = 32768;
 // The size in bytes of the most virtual memory a process can map.
 inline constexpr size_t kMemoryMappingSize = (1UL << 36);  // 64 GB
 // The size in bytes of the pipe's channel.
