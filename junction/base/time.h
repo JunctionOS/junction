f2cef48e2b41dae69b0140dc7317bf04b8becd38
Adam Belay <adam@abelay.me>
2023-10-10 20:30:30 +0000

    add missing inlines

--- junction/base/time.h
+++ junction/base/time.h
@@ -20,23 +20,23 @@ namespace detail {
 inline uint64_t MicroTime() { return microtime(); }
 
 // Converts a Linux timeval into microseconds
-constexpr uint64_t timeval_to_us(const timeval &tv) {
+constexpr inline uint64_t timeval_to_us(const timeval &tv) {
   return tv.tv_sec * kSeconds + tv.tv_usec;
 }
 
 // Converts microseconds into a Linux timeval
-constexpr timeval us_to_timeval(uint64_t us) {
+constexpr inline timeval us_to_timeval(uint64_t us) {
   return timeval{.tv_sec = static_cast<time_t>(us / kSeconds),
                  .tv_usec = static_cast<suseconds_t>(us % kSeconds)};
 }
 
 // Converts a Linux timespec into microseconds
-constexpr uint64_t timespec_to_us(const timespec &ts) {
+constexpr inline uint64_t timespec_to_us(const timespec &ts) {
   return ts.tv_sec * kSeconds + ts.tv_nsec / 1000;
 }
 
 // Converts microseconds into a Linux timespec
-constexpr timespec us_to_timespec(uint64_t us) {
+constexpr inline timespec us_to_timespec(uint64_t us) {
   return timespec{.tv_sec = static_cast<time_t>(us / kSeconds),
                   .tv_nsec = static_cast<long>((us % kSeconds) * 1000)};
 }
