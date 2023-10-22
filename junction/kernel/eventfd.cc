5be3e82c8dec27b16a3d10eef446bfe666cbd406
Adam Belay <adam@abelay.me>
2023-10-22 03:00:05 +0000

    add WakeOnSignal to eventfd

--- junction/kernel/eventfd.cc
+++ junction/kernel/eventfd.cc
@@ -4,6 +4,8 @@ extern "C" {
 #include <sys/eventfd.h>
 }
 
+#include "junction/bindings/rcu.h"
+#include "junction/bindings/wait.h"
 #include "junction/kernel/file.h"
 #include "junction/kernel/proc.h"
 #include "junction/kernel/usys.h"
@@ -21,16 +23,16 @@ constexpr unsigned int kEventFdSupportedFlags =
 
 constexpr size_t kEventFdValSize = 8;
 
-class EventFdFile : public File {
+class EventFDFile : public File, public rt::RCUObject {
  public:
-  EventFdFile(unsigned int initval, int flags) noexcept
+  EventFDFile(unsigned int initval, int flags) noexcept
       : File(FileType::kNormal, flags & kEventFdSupportedFlags,
              kModeReadWrite) {
     val_ = initval;
     get_poll_source().Set(kPollOut);
   }
 
-  ~EventFdFile() = default;
+  ~EventFDFile() = default;
   Status<size_t> Read(std::span<std::byte> buf,
                       [[maybe_unused]] off_t *off) override;
   Status<size_t> Write(std::span<const std::byte> buf,
@@ -45,17 +47,19 @@ class EventFdFile : public File {
   static_assert(sizeof(val_) == kEventFdValSize);
 };
 
-Status<size_t> EventFdFile::Read(std::span<std::byte> buf,
+Status<size_t> EventFDFile::Read(std::span<std::byte> buf,
                                  [[maybe_unused]] off_t *off) {
   if (buf.size_bytes() < kEventFdValSize) return MakeError(EINVAL);
 
   uint64_t *out = reinterpret_cast<uint64_t *>(buf.data());
 
+  WakeOnSignal signaled(lock_);
   rt::SpinGuard guard(lock_);
   if (!val_) {
     if (is_nonblocking()) return MakeError(EAGAIN);
-    guard.Park(queue_, [this] { return val_ != 0; });
+    guard.Park(queue_, [this, &signaled] { return val_ != 0 || signaled; });
   }
+  if (signaled) return MakeError(EINTR);
 
   if (is_semaphore()) {
     *out = 1;
@@ -66,27 +70,33 @@ Status<size_t> EventFdFile::Read(std::span<std::byte> buf,
   }
 
   if (val_ == 0) get_poll_source().Clear(kPollIn);
+  if (val_ < UINT64_MAX) get_poll_source().Set(kPollOut);
   queue_.WakeAll();
   return kEventFdValSize;
 }
 
-Status<size_t> EventFdFile::Write(std::span<const std::byte> buf,
+Status<size_t> EventFDFile::Write(std::span<const std::byte> buf,
                                   [[maybe_unused]] off_t *off) {
   if (buf.size_bytes() < kEventFdValSize) return MakeError(EINVAL);
   uint64_t val = *reinterpret_cast<const uint64_t *>(buf.data());
   if (val == UINT64_MAX) return MakeError(EINVAL);
   if (!val) return kEventFdValSize;
 
+  WakeOnSignal signaled(lock_);
   rt::SpinGuard guard(lock_);
 
   // check for overflow
   if (val + val_ < val) {
     if (is_nonblocking()) return MakeError(EAGAIN);
-    guard.Park(queue_, [this, val] { return val + val_ >= val; });
+    guard.Park(queue_, [this, &signaled, val] {
+      return val + val_ >= val || signaled;
+    });
   }
+  if (signaled) return MakeError(EINTR);
 
   val_ += val;
   get_poll_source().Set(kPollIn);
+  if (val_ == UINT64_MAX) get_poll_source().Clear(kPollOut);
   queue_.WakeAll();
   return kEventFdValSize;
 }
@@ -94,13 +104,15 @@ Status<size_t> EventFdFile::Write(std::span<const std::byte> buf,
 }  // namespace
 
 long usys_eventfd2(unsigned int initval, int flags) {
-  auto efd = std::make_shared<EventFdFile>(initval, flags);
+  std::shared_ptr<EventFDFile> efd(new EventFDFile(initval, flags),
+                                   rt::RCUDeleter<EventFDFile>());
   return myproc().get_file_table().Insert(std::move(efd),
                                           (flags & kFlagCloseExec) > 0);
 }
 
 long usys_eventfd(unsigned int initval) {
-  auto efd = std::make_shared<EventFdFile>(initval, 0);
+  std::shared_ptr<EventFDFile> efd(new EventFDFile(initval, 0),
+                                   rt::RCUDeleter<EventFDFile>());
   return myproc().get_file_table().Insert(std::move(efd));
 }
 
