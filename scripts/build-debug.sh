edde4feb81f2510a062eeeb136b26089a37250d3
bsdinis <bsdinis@mit.edu>
2023-11-20 20:21:39 +0000

    [junction/scripts]: add a build-debug scripts
    Pain point: sometimes building in debug is useful (faster compilation +
    better debuggability). We might also want to have debug builds alongside
    release builds.
    
    Solution: create a build-debug.sh script that builds on
    `${ROOT}/build-debug`

---
+++ scripts/build-debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+
+# Globals
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+ROOT_DIR=${SCRIPT_DIR}/../
+BUILD_DIR=${ROOT_DIR}/build-debug
+BIN_DIR=${ROOT_DIR}/bin
+CMAKE=${BIN_DIR}/bin/cmake
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+$CMAKE -DCMAKE_BUILD_TYPE=Debug ..
+make -j `nproc`
