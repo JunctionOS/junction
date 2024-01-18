22093177ae960baa917ea90a7de6d0a5dbdf392a
bsdinis <bsdinis@mit.edu>
2024-01-18 19:14:45 +0000

    [junction/test]: cleanup and consolidate tests by having cli options

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,49 +1,65 @@
 #!/bin/bash
+
+function usage() {
+    echo "usage: scripts/test.sh [-d|--debug] [-h|--help] [--no-uintr] [regex]" >&2
+    exit 255
+}
+
+NOUINTR=""
+REGEX=""
+MODE="Release"
+
+for arg in "$@"; do
+    shift
+    case "${arg}" in
+        '--help'|'-h') usage ;;
+        '--debug'|'-d') MODE="Debug" ;;
+        '--no-uintr') NOUINTR="nouintr" ;;
+        *)
+            if [[ -z ${REGEX} ]]; then
+                REGEX=${arg}
+            else
+                usage
+            fi;;
+    esac
+done
+
 set -x
 
-REGEX=""
-if [ $# -eq 1 ]; then
-  REGEX="$1"
-fi
-
 # Globals
-SCRIPT_DIR=$(dirname $(readlink -f $0))
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 ROOT_DIR=${SCRIPT_DIR}/../
-BUILD_DIR=${ROOT_DIR}/build
+if [[ "${MODE}" == "Release" ]] ; then
+    BUILD_DIR=${ROOT_DIR}/build
+else
+    BUILD_DIR=${ROOT_DIR}/build-debug
+fi
 CALADAN_DIR=${ROOT_DIR}/lib/caladan
 TEST_DIR=${BUILD_DIR}/junction
 BIN_DIR=${ROOT_DIR}/bin
 CTEST=${BIN_DIR}/bin/ctest
 
 # Start Caladan
-pushd $CALADAN_DIR
+pushd "${CALADAN_DIR}" || exit 255
 (sudo pkill iokerneld && sleep 2) || true
-sudo scripts/setup_machine.sh
+sudo scripts/setup_machine.sh ${NOUINTR}
 sudo ./iokerneld simple nobw noht no_hw_qdel -- --allow 00:00.0 --vdev=net_tap0 > /tmp/iokernel.log 2>&1 &
-iok_pid=$!
 while ! grep -q 'running dataplan' /tmp/iokernel.log; do
   sleep 0.3
   # make sure it is still alive
   pgrep iokerneld > /dev/null
 done
 reset
-popd
-
-# Prepare test environment
-rm -rf /tmp/junction
-mkdir /tmp/junction
+popd || exit 255
 
 # Run tests
-cd $TEST_DIR
+pushd "${TEST_DIR}" || exit 255
 export GTEST_COLOR=1
 if [ "${REGEX}" = "" ]; then
-  sudo $CTEST --output-on-failure --verbose --timeout 120
+  sudo "${CTEST}" --output-on-failure --verbose --timeout 120
 else
-  sudo $CTEST --output-on-failure --verbose --timeout 120 --tests-regex "${REGEX}"
+  sudo "${CTEST}" --output-on-failure --verbose --timeout 120 --tests-regex "${REGEX}"
 fi
-
-# Cleanup test state
-rm -rf /tmp/junction
 
 # Stop Caladan
 sudo pkill iokerneld
