229b1cdb7355c27775e9a5e2123a249292b60fe1
bsdinis <bsdinis@mit.edu>
2024-03-08 20:10:13 +0000

    [junction/viz]: prof vizualization fixes
    - fix plot_timings to use first event as start
    - remove support for json (json sucks)
    - make awk do the deltas
    - add support for scaling (us|ms|s)

--- scripts/viz/plot_timings.py
+++ scripts/viz/plot_timings.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import click
 import io
-import json
 import matplotlib.pyplot as plt
 import pandas
 import sys
@@ -14,80 +13,36 @@ def load_table_from_file(file):
         return pandas.read_table(
             file, header=None, names=[
                 'time', 'trace'], delim_whitespace=True)
-    elif file.endswith(".json"):
-        with open(file, 'r') as fp:
-            json_obj = json.load(fp)
-            json_list = sorted(
-                [(float(k), v) for k, v in json_obj.items()], key=lambda x: float(x[0]))
-
-        return pandas.DataFrame(json_list, columns=['time', 'trace'])
     else:
         raise ValueError(
-            'file must be a dat, csv or json file. found {}'.format(file))
+            'file must be a dat/csv file. found {}'.format(file))
 
 
 def load_table_from_stdin():
-    stdin = sys.stdin.read()
-    json_exception = None
     try:
-        json_obj = json.loads(stdin)
-        json_list = sorted(
-            [(float(k), v) for k, v in json_obj.items()], key=lambda x: float(x[0]))
-
-        if not json_list:
-            raise ValueError('empty json')
-
-        return pandas.DataFrame(json_list, columns=['time', 'trace'])
-    except BaseException as e:
-        json_exception = e
-        pass
-
-    try:
-        fp = io.StringIO(stdin)
         return pandas.read_table(
-            fp, header=None, names=[
+            sys.stdin, header=None, names=[
                 'time', 'trace'], delim_whitespace=True)
     except BaseException as e:
-        try:
-            if json_exception:
-                raise json_exception
-        except BaseException:
-            logging.exception('')
-
         logging.exception('')
-        raise ValueError('could not load stdin as csv, dat or json, aborting')
-
-
-def compute_partials(df):
-    # add dummy event START
-    df.loc[-1] = [0, 'START']
-    df.index = df.index + 1
-    df = df.sort_index()
-
-    # compute partial diffs
-    df['time'] = df['time'].diff()
-
-    # drop dummy event
-    df = df.drop(0)
-
-    return df
+        raise ValueError('could not load stdin as dat/csv, aborting')
 
 
 def transpose_dataframe(df):
-    renaming_dict = {(idx + 1): label for (idx, label)
-                     in enumerate(df['trace'])}
+    renaming_dict = {(idx): label for (idx, label) in enumerate(df['trace'])}
     df = df.drop('trace', axis=1)
     df.rename(renaming_dict, inplace=True)
 
     return df.transpose()
 
 
-def plot_dataframe(df, output_filename):
-    df.plot(
-        kind='barh',
-        stacked=True,
-        title='Timing breakdown',
-        xlabel='time (s)').legend(loc='upper center', bbox_to_anchor=(.5, 2))
+def plot_dataframe(df, output_filename, scale):
+    df.plot(kind='barh',
+            stacked=True,
+            title='Timing breakdown',
+            xlabel='time ({})'.format(scale)).legend(loc='upper center',
+                                                     bbox_to_anchor=(.5,
+                                                                     2))
 
     plt.savefig(output_filename,
                 dpi=100,
@@ -99,7 +54,8 @@ def plot_dataframe(df, output_filename):
                help="plot a bar chart with the timings obtained from timings.awk")
 @click.argument("file", type=click.Path(exists=True), required=False)
 @click.option("--output", "-o", type=click.Path(), required=False)
-def plot(file, output):
+@click.option("--scale", "-s", type=click.STRING, default="s")
+def plot(file, output, scale):
     try:
         if file:
             df = load_table_from_file(file)
@@ -113,9 +69,9 @@ def plot(file, output):
         logging.exception('failed to load dataframe')
         return
 
-    df = compute_partials(df)
+    print(df)
     df = transpose_dataframe(df)
-    plot_dataframe(df, output)
+    plot_dataframe(df, output, scale)
 
 
 if __name__ == '__main__':
--- scripts/viz/timings.awk
+++ scripts/viz/timings.awk
@@ -1,23 +1,33 @@
 #!awk -f
+#
+# usage: timings.awk -v regex=[;-separated list of regex] -v scale=[ms|milli|us|micro|s|sec]
 
 BEGIN {
-    if ( length(output_mode) == 0 ) {
-        output_mode = "json"
-    }
-
     if ( length(regex) == 0 ) {
         regex="."
     }
-
-    if ( output_mode != "json" && output_mode != "dat" ) {
-        print "invalid output mode: `" output_mode "`"
-        print "usage: timings.awk -v output_mode=[json|dat] -v regex=[;-separated list of regex]"
-        exit
+    if ( length(scale) == 0 ) {
+        scale="s"
     }
 
     split(regex, regexlist, ";")
 
+    if ( scale != "s" && scale != "sec" && scale != "secs" && scale != "ms" && scale != "milli" && scale != "millis" && scale != "us" && scale != "micro" && scale != "micros" ) {
+        print "invalid scale: `" scale "`"
+        print "usage: timings.awk -v regex=[;-separated list of regex] -v scale=[ms|milli|us|micro|s|sec]"
+        exit
+    }
+
+    if ( scale == "s" || scale == "sec" || scale == "secs") {
+        scale = "s"
+    } else if ( scale == "ms" || scale == "milli" || scale == "millis") {
+        scale = "ms"
+    } else if ( scale == "us" || scale == "micro" || scale == "micros") {
+        scale = "us"
+    }
+
     n_records = 0
+    prev_time = 0
 }
 
 # return 0 if no match is found
@@ -46,26 +56,24 @@ function array_match(string, regex_array) {
     n_records += 1
     time=gensub(/\]/, "", "g", $2);
 
+    if ( n_records > 1 ) {
+        trace[n_records] = message
+        delta[n_records] = time - prev_time
+    }
 
-    trace[time] = message
-    timings[n_records] = time
+    prev_time = time
 }
 
 END {
-    asort(timings)
-    n_timings = length(timings)
-    if (output_mode == "json") {
-        printf "{\n"
-        for ( i = 1; i < n_timings; i++) {
-            printf "\t\"%s\": \"%s\",\n", timings[i], trace[timings[i]]
-        }
-        if ( n_timings > 0 ) {
-            printf "\t\"%s\": \"%s\"\n", timings[n_timings], trace[timings[n_timings]]
-        }
-        printf "}\n"
-    } else {
-        for ( i = 1; i <= n_timings; i++) {
-            printf "%s  \"%s\"\n", timings[i], trace[timings[i]]
+    for ( i = 2; i <= n_records; i++ ) {
+        time_delta = timings[i] - timings[i - 1]
+        message = trace[timings[i]]
+        if (scale == "s") {
+            printf "%.7f\t\"%s\"\n", delta[i] , trace[i]
+        } else if ( scale == "ms") {
+            printf "%.4f\t\"%s\"\n", delta[i] * 1000, trace[i]
+        } else if ( scale == "us") {
+            printf "%.1f\t\"%s\"\n", delta[i] * 1000000, trace[i]
         }
     }
 }
