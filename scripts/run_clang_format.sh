c72b52e8dfc0047dc0cfc739ace169e0b26636b5
bsdinis <bsdinis@mit.edu>
2024-03-11 15:20:50 +0000

    [junction/tooling]: add comment about style guide

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -6,4 +6,5 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 ROOT_DIR=${SCRIPT_DIR}/../
 CODE_DIR=${ROOT_DIR}/junction
 
+# style is based on Google's style, defined in the .clang-format file
 find "${CODE_DIR}" -iname '*.h' -o -iname '*.cc' | xargs clang-format-16 -i
