dccf32a725970811e2ba4d2db8f61c291ea7d2d6
Josh Fried <joshuafried@gmail.com>
2024-05-08 13:53:18 +0000

    nit

--- junction/base/string.h
+++ junction/base/string.h
@@ -11,30 +11,28 @@ namespace junction {
 
 // split creates a vector of string views delineated by a seperator character
 inline std::vector<std::string_view> split(
-    std::string_view text, char sep,
-    size_t max = std::numeric_limits<size_t>::max()) {
+    std::string_view text, char sep, std::optional<size_t> max = std::nullopt) {
   std::vector<std::string_view> tokens;
   std::string_view::size_type start = 0, end;
 
   while ((end = text.find(sep, start)) != std::string_view::npos) {
     tokens.push_back(text.substr(start, end - start));
     start = end + 1;
-    if (tokens.size() >= max) break;
+    if (max && tokens.size() >= *max) break;
   }
   tokens.push_back(text.substr(start));
   return tokens;
 }
 
 inline std::vector<std::string_view> rsplit(
-    std::string_view text, char sep,
-    size_t max = std::numeric_limits<size_t>::max()) {
+    std::string_view text, char sep, std::optional<size_t> max = std::nullopt) {
   std::vector<std::string_view> tokens;
   std::string_view::size_type start, end = text.size() - 1;
 
   while ((start = text.rfind(sep, end)) != std::string_view::npos) {
     tokens.push_back(text.substr(start + 1, end - start));
     end = start - 1;
-    if (tokens.size() >= max) break;
+    if (max && tokens.size() >= *max) break;
     if (start == 0) break;
   }
   if (end + 1 > 0) tokens.push_back(text.substr(0, end + 1));
