55a0a8c761d964e27a996c4a479bd6aee3375f44
bsdinis <bsdinis@mit.edu>
2024-08-28 15:58:51 +0100

    [junction/java-samples]: update java samples

--- junction/samples/snapshots/java/matmul/MatMul.java
+++ junction/samples/snapshots/java/matmul/MatMul.java
@@ -1,5 +1,8 @@
 package profiling;
 
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
+
 import java.util.Random;
 
 import java.io.BufferedReader;
@@ -13,7 +16,7 @@ import com.sun.jna.Native;
 
 class MatMul {
     public static final int DEFAULT_N = 100;
-    public static final int DEFAULT_EXP = 20;
+    public static final int WARMUP = 10;
 
     public interface CStdLib extends Library {
         int syscall(int number, Object... args);
@@ -37,15 +40,15 @@ class MatMul {
     }
 
     // return time
-    private static long exp_round(int[][] src, int[][] dst, int n) {
+    private static long mul(int[][] a, int[][] b, int[][] result, int n) {
         long start = System.nanoTime();
         for (int i = 0; i < n; i += 1) {
             for (int j = 0; j < n; j += 1) {
                 int res_i_j = 0;
                 for (int k = 0; k < n; k += 1) {
-                    res_i_j += src[i][k] * src[k][j];
+                    res_i_j += a[i][k] * b[k][j];
                 }
-                dst[i][j] = res_i_j;
+                result[i][j] = res_i_j;
             }
         }
         return System.nanoTime() - start;
@@ -63,31 +66,24 @@ class MatMul {
                     continue;
                 }
 
-                String[] args = cmd.split(" ");
-                if (args.length != 2) {
-                    throw new IllegalStateException("need <n> <exp>");
-                }
+                JSONObject args = (JSONObject) JSONValue.parse(cmd);
 
-                int n = Integer.parseInt(args[0]);
-                int exp = Integer.parseInt(args[1]);
+                int n = Math.toIntExact((long) args.get("N"));
 
                 // setup
                 Random rand = new Random();
 
                 int[][] a = new int[n][n];
                 int[][] b = new int[n][n];
+                int[][] res = new int[n][n];
                 for (int i = 0; i < n; i += 1) {
                     for (int j = 0; j < n; j += 1) {
                         a[i][j] = rand.nextInt(10);
-                        b[i][j] = i == j ? 1 : 0;
+                        b[i][j] = rand.nextInt(10);
                     }
                 }
 
-                for (int rep = 0; rep  < exp; rep  += 1) {
-                    int[][] src = (rep % 2 == 0) ? a : b;
-                    int[][] dst = (rep % 2 == 0) ? b : a;
-                    exp_round(src, dst, n);
-                }
+                mul(a, b, res, n);
 
                 writeResp(p, "OK");
             }
@@ -99,9 +95,8 @@ class MatMul {
 
     public static void main(String[] args) {
         int n = MatMul.DEFAULT_N;
-        int exp = MatMul.DEFAULT_EXP;
         if (args.length > 2) {
-            System.err.println("usage: java MatMul.java <N> <exp>");
+            System.err.println("usage: java MatMul.java <N>");
             return;
         } else if (args.length > 0) {
             if (args[0].equals("--new_version")) {
@@ -110,9 +105,6 @@ class MatMul {
             }
 
             n = Integer.parseInt(args[0]);
-            if (args.length == 2) {
-                exp = Integer.parseInt(args[1]);
-            }
         }
 
         CStdLib c =  Native.load("c", CStdLib.class);
@@ -120,31 +112,29 @@ class MatMul {
         // get pid of the java program
         long pid = c.getpid();
 
-        System.out.println("raising " + n + "x" + n + " to the " + exp  + " power");
+        System.out.println("multiplying 2 " + n + "x" + n + " matrices");
         long t_start = System.nanoTime();
 
         Random rand = new Random();
 
         int[][] a = new int[n][n];
         int[][] b = new int[n][n];
+        int[][] result = new int[n][n];
         for (int i = 0; i < n; i += 1) {
             for (int j = 0; j < n; j += 1) {
                 a[i][j] = rand.nextInt(10);
-                b[i][j] = i == j ? 1 : 0;
+                b[i][j] = rand.nextInt(10);
             }
         }
 
         long t_setup = System.nanoTime();
         System.out.println("setup done in " + ((t_setup - t_start) / 1000) + "us");
 
-        long[] warmup = new long[exp - 1];
+        long[] warmup = new long[WARMUP];
 
         // warmup
-        for (int rep = 0; rep < exp - 1; rep += 1) {
-            int[][] src = (rep % 2 == 0) ? a : b;
-            int[][] dst = (rep % 2 == 0) ? b : a;
-
-            warmup[rep] = exp_round(src, dst, n);
+        for (int rep = 0; rep < WARMUP; rep += 1) {
+            warmup[rep] = mul(a, b, result, n);
         }
 
         System.out.println("warmup done, snapshotting");
@@ -156,26 +146,21 @@ class MatMul {
         c.syscall(62, pid, 19);
 
         // call
-        long restored_time = 0;
-        for (int rep = exp - 1; rep < exp; rep += 1) {
-            int[][] src = (rep % 2 == 0) ? a : b;
-            int[][] dst = (rep % 2 == 0) ? b : a;
-            restored_time = exp_round(src, dst, n);
-        }
+        long restored_time = restored_time = mul(a, b, result, n);
 
         // Building the desired output line
-        StringBuilder result = new StringBuilder();
-        result.append("DATA  {\"warmup\": [");
+        StringBuilder output = new StringBuilder();
+        output.append("DATA  {\"warmup\": [");
         for (int i = 0; i < warmup.length; i++) {
             if (i > 0) {
-                result.append(", ");
+                output.append(", ");
             }
-            result.append(warmup[i]);
+            output.append(warmup[i]);
         }
-        result.append("], \"cold\": [").append(restored_time).append("], \"program\": \"java_matmul\"}");
+        output.append("], \"cold\": [").append(restored_time).append("], \"program\": \"java_matmul\"}");
 
         // Print the result
-        System.out.println(result.toString());
+        System.out.println(output.toString());
 
         // Exit immediately
         System.out.flush();
