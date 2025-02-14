04ad96e26fafaa13efffbf4a636a0de956692e4d
Josh Fried <joshuafried@gmail.com>
2025-02-14 00:50:37 -0500

    bump cmake

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -9,10 +9,15 @@ BIN_DIR=${ROOT_DIR}/bin
 mkdir -p $BIN_DIR
 cd $BIN_DIR
 
+CMAKE_VER=3.31.5
+
+EXPECTED_SHA="79ef8d796c0dc16f75b9fb63ddc3fd4191c641db3cd08cb08bafd9e993797596"
+FILE="$BIN_DIR/bin/cmake"
+
 # Install a specific version of cmake
-if [ ! -f $BIN_DIR/bin/cmake ]; then
-  wget https://github.com/Kitware/CMake/releases/download/v3.24.2/cmake-3.24.2-linux-x86_64.sh
-  chmod +x cmake-3.24.2-linux-x86_64.sh
-  ./cmake-3.24.2-linux-x86_64.sh --skip-license
-  rm cmake-3.24.2-linux-x86_64.sh
+if [ ! -f "$FILE" ] || [ "$(shasum -a 256 "$FILE" | awk '{print $1}')" != "$EXPECTED_SHA" ]; then
+  wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VER}/cmake-${CMAKE_VER}-linux-x86_64.sh
+  chmod +x cmake-${CMAKE_VER}-linux-x86_64.sh
+  ./cmake-${CMAKE_VER}-linux-x86_64.sh --skip-license
+  rm cmake-${CMAKE_VER}-linux-x86_64.sh
 fi
